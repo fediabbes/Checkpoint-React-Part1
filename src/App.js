debc416cd1e2a5ae92103cd5fe55feace815558f
Fedi abbes <abbesfedy24@gmail.com>
2021-12-06 17:43:54 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Newform from   "./newform.js" 
+
+
+function App() {
+  return (
+    <div className = 'app'>
+
+      <Newform />
+      
+    </div>
+  );
+}
+
+export default App;
